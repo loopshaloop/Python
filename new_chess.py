23344751697e5ec084bb9286bf10c9753bfa4218
loopshaloop <ohad271005@gmail.com>
2023-06-02 16:14:45 +0300

    modified:   new_chess.py

--- new_chess.py
+++ new_chess.py
@@ -4,9 +4,13 @@ from copy import deepcopy
 import os
 import time
 
+
+#A shorter function for clearing the console.
 def clear():
     os.system('cls')
 
+
+#An Enum class for the recurring chess elements.
 class Chess(enum.Enum):
     PAWN = 0
     ROOK = 1
@@ -20,6 +24,8 @@ class Chess(enum.Enum):
     LEGAL_CHARS = "abcdefgh"
     START_FEN = "RHBQKBHR/PPPPPPPP/8/8/8/8/pppppppp/rhbqkbhr w KQkq - 0 1"
 
+
+#The main function that starts the game loop.
 def play():
     clear()
     print("This is not ready! Try it again later!")
@@ -27,6 +33,9 @@ def play():
     clear()
     exit()
 
+
+#The function that let's you choose whether you want to start the game,
+#quit the program, or review the tutorial.
 def menu():
     print("To start playing, type \'play\'!\nTo quit, type \'quit\'!\nFor a tutorial, enter \'tutorial\'!")
     inp = input()
@@ -52,6 +61,8 @@ def menu():
             clear()
             return 1
 
+
+#The function called to start up the program and present the summary of the program.
 def start_up():
     clear()
     print("Hello! Welcome to Chess!")
@@ -64,4 +75,7 @@ def start_up():
     while choice == 1:
         choice = menu()
 
+
+#This is where everything begins, this calls the start_up function to start up the game
+#on the program start.
 start_up()
